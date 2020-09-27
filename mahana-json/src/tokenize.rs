c5ce1a7ac5254b24f58211d09b3cea46b81760a3
komi1230 <yukke.konan@gmail.com>
2020-09-28 00:31:40 +0900

    Implement tokenize for string and brace

--- mahana-json/src/tokenize.rs
+++ mahana-json/src/tokenize.rs
@@ -1,3 +1,36 @@
+pub fn tokenize(data: String) -> Vec<String> {
+    let mut content: Vec<String> = Vec::new();
+    let mut cs = data.chars();
+
+    loop {
+        let chr = cs.next();
+
+        match chr {
+            None => break,
+            Some(c) => {
+                // brace
+                if c == '{' || c == '}' {
+                    content.push(c.to_string());
+                }
+
+                // string
+                if c == '\"' {
+                    let mut word = String::new();
+                    while let Some(next_c) = cs.next() {
+                        if next_c == '\"' {
+                            break;
+                        } else {
+                            word.push(next_c);
+                        }
+                    }
+                    content.push(word);
+                }
+            }
+        }
+    }
+    content
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -5,9 +38,22 @@ mod tests {
     use std::io::Read;
 
     fn get_sample() -> String {
-        let mut file = File::open("./sample/index.json").unwrap();
+        let mut file = File::open("./sample/small.json").unwrap();
         let mut content = String::new();
         file.read_to_string(&mut content).unwrap();
         content
+    }
+
+    #[test]
+    fn test_get_sample() {
+        let data = get_sample();
+        assert!(data.len() > 0);
+    }
+
+    #[test]
+    fn test_tokenize() {
+        let data = get_sample();
+        let tokens = tokenize(data);
+        assert_eq!(tokens, vec!["hoge"]);
     }
 }
