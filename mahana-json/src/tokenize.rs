6ff2a2af60f35c63ecfd8767e3c7ec340297b230
komi1230 <yukke.konan@gmail.com>
2020-09-28 23:12:41 +0900

    Add type checker for number and boolean

--- mahana-json/src/tokenize.rs
+++ mahana-json/src/tokenize.rs
@@ -1,6 +1,21 @@
+pub fn is_number(seq: String) -> bool {
+    match seq.parse::<i32>() {
+        Ok(_) => true,
+        Err(_) => match seq.parse::<f64>() {
+            Ok(_) => true,
+            Err(_) => false,
+        },
+    }
+}
+
+pub fn is_bool(seq: String) -> bool {
+    seq == "true".to_string() || seq == "false".to_string()
+}
+
 pub fn tokenize(data: String) -> Vec<String> {
     let mut content: Vec<String> = Vec::new();
     let mut cs = data.chars();
+    let special_chars = ['{', '}', '[', ']', ':'];
     let mut numbers: Vec<char> = (0..10)
         .map(|item| std::char::from_digit(item as u32, 10).unwrap())
         .collect();
@@ -13,7 +28,7 @@ pub fn tokenize(data: String) -> Vec<String> {
             None => break,
             Some(c) => {
                 // brace
-                if c == '{' || c == '}' || c == ':' {
+                if special_chars.contains(&c) {
                     content.push(c.to_string());
                 }
 
@@ -90,6 +105,22 @@ mod tests {
     fn test_get_sample() {
         let data = get_sample();
         assert!(data.len() > 0);
+    }
+
+    #[test]
+    fn test_is_number() {
+        assert!(is_number("3.2".to_string()));
+        assert!(is_number("-20".to_string()));
+        assert!(!is_number("10.9.8".to_string()));
+        assert!(!is_number("hello".to_string()))
+    }
+
+    #[test]
+    fn test_is_bool() {
+        assert!(is_bool("true".to_string()));
+        assert!(is_bool("false".to_string()));
+        assert!(!is_bool("hello".to_string()));
+        assert!(!is_bool("114514".to_string()));
     }
 
     #[test]
