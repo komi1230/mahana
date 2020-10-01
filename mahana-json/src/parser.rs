2cf72ecaed5b982d3fe927c6798316a29c44a04c
komi1230 <yukke.konan@gmail.com>
2020-10-01 22:36:44 +0900

    Initialize parser without tokenizer

--- mahana-json/src/parser.rs
+++ mahana-json/src/parser.rs
@@ -1,1 +1,10 @@
 use std::collections::HashMap;
+
+use crate::value::{Number, Value};
+
+pub fn parse(seq: String) -> Result<HashMap<String, Value>, String> {
+    let result: HashMap<String, Value> = HashMap::new();
+    let cs = seq.chars();
+    let braces = ['{', '}'];
+    Ok(result)
+}
