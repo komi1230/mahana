71da1506fa09d0149fba872d39aca6cfd205e83a
komi1230 <yukke.konan@gmail.com>
2020-10-04 18:10:37 +0900

    Add array parser

--- mahana-json/src/parser.rs
+++ mahana-json/src/parser.rs
@@ -2,9 +2,64 @@ use std::collections::HashMap;
 
 use crate::{Number, Value};
 
+pub fn parse_arr(mut cs: std::str::Chars) -> Result<(Value, std::str::Chars), String> {
+    let numbers: Vec<char> = (0..9)
+        .map(|item| std::char::from_digit(item as u32, 10).unwrap())
+        .collect();
+    let mut content: Vec<Value> = Vec::new();
+    while let Some(c) = cs.next() {
+        // string
+        if c == '"' {
+            let mut word = String::new();
+            while let Some(next_c) = cs.next() {
+                if next_c == '"' {
+                    content.push(Value::String(word));
+                    break;
+                } else {
+                    word.push(c);
+                }
+            }
+        }
+        // number
+        if numbers.contains(&c) {
+            let mut num = String::new();
+            while let Some(next_c) = cs.next() {
+                if next_c == ',' {
+                    break;
+                } else if next_c == ']' {
+                    return Ok((Value::Array(content), cs));
+                } else {
+                    num.push(next_c);
+                }
+            }
+            if let Ok(i) = num.parse::<i32>() {
+                content.push(Value::Number(Number::Int(i)))
+            } else if let Ok(f) = num.parse::<f64>() {
+                content.push(Value::Number(Number::Float(f)));
+            } else {
+                return Err("Parse Error".to_string());
+            }
+        }
+        // array
+        if c == '[' {
+            if let Ok((result, tmp_cs)) = parse_arr(cs.clone()) {
+                cs = tmp_cs;
+                content.push(result);
+            } else {
+                return Err("Parse Error".to_string());
+            }
+        }
+    }
+    Ok((Value::Array(content), cs))
+}
+
 pub fn parse(seq: String) -> Result<HashMap<String, Value>, String> {
     let result: HashMap<String, Value> = HashMap::new();
-    let cs = seq.chars();
+    let mut cs = seq.chars();
     let braces = ['{', '}'];
+    while let Some(c) = cs.next() {
+        // braces
+        if braces.contains(&c) {}
+    }
     Ok(result)
 }
