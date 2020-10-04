54b75f378bf181a210a0912209b26c84a26c781b
komi1230 <yukke.konan@gmail.com>
2020-10-04 23:41:08 +0900

    Set number reader

--- mahana-json/src/util.rs
+++ mahana-json/src/util.rs
@@ -1,25 +1,41 @@
 use std::str::Chars;
 
-fn is_number(seq: &str) -> bool {
+use crate::Number;
+
+fn read_number(seq: String) -> Result<Number, String> {
     match seq.parse::<i32>() {
-        Ok(_) => true,
+        Ok(i) => Ok(Number::Int(i)),
         Err(_) => match seq.parse::<f64>() {
-            Ok(_) => true,
-            Err(_) => false,
+            Ok(f) => Ok(Number::Float(f)),
+            Err(_) => Err("Not number".to_string()),
         },
     }
 }
 
-// expect comma
 pub fn expect_comma(mut cs: Chars) -> Result<Chars, String> {
     while let Some(next_c) = cs.next() {
         if next_c == ' ' {
             continue;
         } else if next_c == ',' {
             break;
-        } else {
-            return Err("Parse Error".to_string());
+        } else if next_c == ']' || next_c == '}' {
+            return Ok(cs);
         }
     }
     Ok(cs)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_number() {
+        let test_num1 = "32".to_string();
+        let test_num2 = "1.23".to_string();
+        let test_num3 = "hello".to_string();
+        assert!(parse_number(test_num1).is_ok());
+        assert!(parse_number(test_num2).is_ok());
+        assert!(parse_number(test_num3).is_err());
+    }
 }
