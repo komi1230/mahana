01d8c54922a8aa4f1951f820ce9a44841437a475
komi1230 <yukke.konan@gmail.com>
2020-10-07 22:50:36 +0900

    Implement parse function

--- mahana-json/src/lib.rs
+++ mahana-json/src/lib.rs
@@ -4,13 +4,14 @@ mod util;
 use std::collections::HashMap;
 
 use parser::parse_object;
-use util::expect_token;
 
+#[derive(Debug, PartialEq)]
 pub enum Number {
     Int(i32),
     Float(f64),
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Value {
     String(String),
     Number(Number),
@@ -20,11 +21,40 @@ pub enum Value {
     Object(HashMap<String, Value>),
 }
 
-// pub fn parse(seq: String) -> Result<HashMap<String, Value>, String> {
-//     let cs = seq.chars();
-//     if let Some(c) = cs.next() {
-//         if c == '{' {}
-//     } else {
-//         return Err("Parse Error".to_string());
-//     }
-// }
+pub fn parse(seq: String) -> Result<HashMap<String, Value>, String> {
+    let mut cs = seq.chars();
+    if let Some(c) = cs.next() {
+        if c == '{' {
+            if let Ok((result, _c)) = parse_object(&mut cs) {
+                if let Value::Object(data) = result {
+                    return Ok(data);
+                } else {
+                    return Err("Parse Error".to_string());
+                }
+            } else {
+                return Err("Parse Error".to_string());
+            }
+        } else {
+            return Err("Parse Error".to_string());
+        }
+    } else {
+        return Err("Parse Error".to_string());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    fn get_json() -> String {
+        let file_path = "sample/small.json";
+        let content = fs::read_to_string(file_path).unwrap();
+        content
+    }
+
+    #[test]
+    fn hello() {
+        println!("{}", get_json());
+    }
+}
