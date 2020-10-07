95ecc1ba7658c796c76cff949109082ef12b8eb1
komi1230 <yukke.konan@gmail.com>
2020-10-07 23:45:15 +0900

    Change type of args in read_number

--- mahana-json/src/util.rs
+++ mahana-json/src/util.rs
@@ -2,7 +2,7 @@ use std::str::Chars;
 
 use crate::Number;
 
-pub fn read_number(seq: String) -> Result<Number, String> {
+pub fn read_number(seq: &str) -> Result<Number, String> {
     match seq.parse::<i32>() {
         Ok(i) => Ok(Number::Int(i)),
         Err(_) => match seq.parse::<f64>() {
@@ -13,14 +13,11 @@ pub fn read_number(seq: String) -> Result<Number, String> {
 }
 
 pub fn expect_token(cs: &mut Chars) -> Result<char, String> {
-    let tokens = [',', ']', '}'];
     while let Some(next_c) = cs.next() {
         if next_c == ' ' {
             continue;
-        } else if tokens.contains(&next_c) {
-            return Ok(next_c);
         } else {
-            break;
+            return Ok(next_c);
         }
     }
     Err("Parse Error".to_string())
@@ -32,9 +29,9 @@ mod tests {
 
     #[test]
     fn test_parse_number() {
-        let test_num1 = "32".to_string();
-        let test_num2 = "1.23".to_string();
-        let test_num3 = "hello".to_string();
+        let test_num1 = "32";
+        let test_num2 = "1.23";
+        let test_num3 = "hello";
         assert!(read_number(test_num1).is_ok());
         assert!(read_number(test_num2).is_ok());
         assert!(read_number(test_num3).is_err());
