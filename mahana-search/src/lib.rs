87a5405fdf7a0f3cd961d01f85081f26d9086e49
komi1230 <yukke.konan@gmail.com>
2020-10-17 22:25:05 +0900

    Define struct

--- mahana-search/src/lib.rs
+++ mahana-search/src/lib.rs
@@ -1,7 +1,17 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
+pub struct Trie<K: Eq + Hash, V: Eq> {
+    value: Option<V>,
+    children: HashMap<K, Trie<K, V>>,
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_hoge() {
+        assert_eq!(1 + 1, 2);
     }
 }
